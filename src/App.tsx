25325d9837a4063ffa72dbdc9265185aa216ccb9
yasinitskyi <yuralee007@gmail.com>
2024-09-02 17:40:43 +0800

    added: calendar main logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from "react";
+import { useImmer } from "use-immer";
+import { zhTW } from "date-fns/locale";
+import { isDayFromPreviousMonth, getLeftoverDays } from "./utils";
+import { Day } from "./components/Day";
+import { Range} from './types/Range'
+import { SliderControl } from "./components/SliderControl";
+import {
+	format,
+	eachDayOfInterval,
+	startOfMonth,
+	endOfMonth,
+	isBefore,
+} from "date-fns";
+import S from "./view/Scaffold";
+import "./App.css";
+
+const dateFormat = "yyyy年M月";
+
+const initialRange: Range = { start: undefined, end: undefined };
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [range, setRange] = useImmer<Range>(initialRange);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	//! Supposed to be dynamic if month switch is supported (added memoization for future month switch support)
+	const currentDate = useMemo(() => new Date(), []);
+	const daysWithinCurrentMonth = useMemo(
+		() =>
+			eachDayOfInterval({
+				start: startOfMonth(currentDate),
+				end: endOfMonth(currentDate),
+			}),
+		[currentDate]
+	);
+	const leftoverDays = useMemo(
+		() => getLeftoverDays(daysWithinCurrentMonth.length, currentDate),
+		[currentDate, daysWithinCurrentMonth]
+	);
+
+	const computeRange = (day: Date) => {
+		// If selection is from previous month - stop execution
+		if (isDayFromPreviousMonth(day)) return;
+
+		// If range is completed and click is fired - start new range
+		if (range.start && range.end) {
+			setRange((draft) => {
+				draft.start = day;
+				draft.end = undefined;
+			});
+
+			return;
+		}
+
+		if (range.start) {
+			// If end date is before start date - reset start date value (mainly set 'before' date to start date in range)
+			if (isBefore(day, range.start)) {
+				setRange((draft) => {
+					draft.start = day;
+				});
+			} else {
+				setRange((draft) => {
+					draft.end = day;
+				});
+			}
+		} else {
+			setRange((draft) => {
+				draft.start = day;
+			});
+		}
+	};
+
+	return (
+		<S.Container>
+			<S.Header>
+				<SliderControl type="back" />
+				<span>{format(currentDate, dateFormat, { locale: zhTW })}</span>
+				<SliderControl type="forth" />
+			</S.Header>
+			<S.Body>
+				{[...leftoverDays, ...daysWithinCurrentMonth].map((day) => (
+					<Day
+						key={day.toDateString()}
+						day={day}
+						onClick={computeRange}
+						range={range}
+					/>
+				))}
+			</S.Body>
+		</S.Container>
+	);
 }
 
-export default App
+export default App;
